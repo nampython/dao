5a299e7d0b0b086b39b7300cfd2d15282c93dea6
Binh Le <binh.le@terralogic.com>
2022-11-29 16:53:15 +0700

    add ErrorCode

--- src/main/java/com/example/Excercise1/utils/ErrorCode.java
+++ src/main/java/com/example/Excercise1/utils/ErrorCode.java
@@ -4,74 +4,57 @@ import java.io.Serializable;
 import java.util.*;
 
 public class ErrorCode implements Serializable {
-    private static final long serialVersionUID = 1L;
-    public final Map<Integer, List<String>> errorMsg = new LinkedHashMap();
+    public final Map<Integer, List<String>> errorMsg = new LinkedHashMap<>();
+    private boolean disableSubmit = false;
+    private String command = null;
 
+    public ErrorCode() {
+    }
+
+    /**
+     * Use to add messages into map according to the code
+     * @param code: the code to identify message
+     * @param msg: msg to display on the client side
+     */
+    public void addErrorMsg(int code, String msg) {
+        if (this.errorMsg.containsKey(code)) {
+            List<String> values = this.errorMsg.get(code);
+            values.add(msg);
+            this.errorMsg.put(code, values);
+        } else {
+            List<String> values = new ArrayList<>();
+            values.add(msg);
+            this.errorMsg.put(code, values);
+        }
+    }
+
+    /**
+     * use to get the length of errors
+     * @return num of errors
+     */
     public int getNumErrors() {
         List<AbstractMap.SimpleEntry<Integer, String>> errorMsgList = this.getErrorMsgListFromMap();
         return errorMsgList.size();
     }
 
-    private List<AbstractMap.SimpleEntry<Integer, String>> getErrorMsgListFromMap() {
-        List<AbstractMap.SimpleEntry<Integer, String>> errorMsgList = new ArrayList();
-        new ArrayList();
-        Set<Integer> keys = this.errorMsg.keySet();
-        Iterator var5 = keys.iterator();
+    /**
+     * convert from map into list of error msg
+     * @return List of error msg
+     */
+    public List<AbstractMap.SimpleEntry<Integer, String>> getErrorMsgListFromMap() {
+        Set<Integer> key = this.errorMsg.keySet();
+        Iterator<Integer> iterator = key.iterator();
+        List<AbstractMap.SimpleEntry<Integer, String>> errorMsgList = new ArrayList<>();
 
-        while(var5.hasNext()) {
-            Integer key = (Integer)var5.next();
-            List<String> messages = (List)this.errorMsg.get(key);
-            Iterator var7 = messages.iterator();
-
-            while(var7.hasNext()) {
-                String msg = (String)var7.next();
-                errorMsgList.add(new AbstractMap.SimpleEntry(key, msg));
+        while (iterator.hasNext()) {
+            Integer var1 = iterator.next();
+            List<String> messages = this.errorMsg.get(var1);
+            Iterator<String> iteratorMsg = messages.iterator();
+            while (iteratorMsg.hasNext()) {
+                String msg = iteratorMsg.next();
+                errorMsgList.add(new AbstractMap.SimpleEntry<>(var1, msg));
             }
-        }
-
+          }
         return errorMsgList;
-    }
-
-    public void addErrorMsg(int code, String msg) {
-        if (this.errorMsg.containsKey(code)) {
-            List<String> values = (List)this.errorMsg.get(code);
-            values.add(msg);
-            this.errorMsg.put(code, values);
-        } else {
-            List<String> values = new ArrayList();
-            values.add(msg);
-            this.errorMsg.put(code, values);
-        }
-
-    }
-    public Enumeration<Integer> getErrorMsgCodeEnum() {
-        Enumeration<Integer> en = Collections.enumeration(this.errorMsg.keySet());
-        return en;
-    }
-
-    public Enumeration<String> getErrorMsgMultiple(int msgNum) {
-        return Collections.enumeration((Collection)this.errorMsg.get(msgNum));
-    }
-
-    public List<String> getErrorMessageList() {
-        List<String> errMsgList = new ArrayList();
-        Enumeration<Integer> errorEnum = this.getErrorMsgCodeEnum();
-        String msg = null;
-        Enumeration<String> msgEnum = null;
-//        int err = false;
-        Integer errCd = null;
-
-        while(errorEnum.hasMoreElements()) {
-            errCd = (Integer)errorEnum.nextElement();
-            int err = errCd;
-            msgEnum = this.getErrorMsgMultiple(err);
-
-            while(msgEnum.hasMoreElements()) {
-                msg = (String)msgEnum.nextElement();
-                errMsgList.add(err + " - " + msg);
-            }
-        }
-
-        return errMsgList;
     }
 }
