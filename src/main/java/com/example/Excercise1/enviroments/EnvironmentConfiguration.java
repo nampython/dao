5487646e8b89eb2681b2069f08e6e8bae5623c84
Binh Le <binh.le@terralogic.com>
2022-12-02 09:31:39 +0700

    add environment configuration

---
+++ src/main/java/com/example/Excercise1/enviroments/EnvironmentConfiguration.java
@@ -0,0 +1,21 @@
+package com.example.Excercise1.enviroments;
+
+import org.springframework.core.env.AbstractEnvironment;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.util.Properties;
+import org.springframework.util.ResourceUtils;
+public class EnvironmentConfiguration  {
+    public static Properties properties() {
+        Properties properties = new Properties();
+        try {
+            File file = ResourceUtils.getFile("classpath:application.properties");
+            InputStream in = Files.newInputStream(file.toPath());
+            properties.load(in);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return properties;
+    }
+}
