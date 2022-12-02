4f0003a18ff6f85e59b63955fb20aa3ce9b81650
Binh Le <binh.le@terralogic.com>
2022-12-02 10:44:48 +0700

    Message: add exception for GeneratedSqlQueryException clas and edit some constants

---
+++ src/main/java/com/example/Excercise1/exceptions/GeneratedSqlQueryException.java
@@ -0,0 +1,11 @@
+package com.example.Excercise1.exceptions;
+
+public class GeneratedSqlQueryException extends RuntimeException {
+    public GeneratedSqlQueryException(String message) {
+        super(message);
+    }
+
+    public GeneratedSqlQueryException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
