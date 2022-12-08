60c3e043f8e47f539c8e31245081093a3526eb3c
NamQuang13 <dragonsangsang10a1@gmail.com>
2022-12-08 20:22:18 +0700

    Message: Extract Variable by add additional checkInstanceVariable

--- src/main/java/com/example/Excercise1/persistence/CommonEntities.java
+++ src/main/java/com/example/Excercise1/persistence/CommonEntities.java
@@ -3,6 +3,7 @@ package com.example.Excercise1.persistence;
 import com.example.Excercise1.exceptions.GetParamsException;
 import com.example.Excercise1.exceptions.ProcessClearException;
 import com.example.Excercise1.exceptions.ProcessParseSqlException;
+
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -13,11 +14,11 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Objects;
+
 import static com.example.Excercise1.persistence.ProcessDatabase.*;
 import static com.example.Excercise1.constants.ResultCode.*;
 
 public class CommonEntities {
-
     public static final String RESULT_CODE_VARIABLE = "resultCode";
     public static final String RESULT_CODE_MESSAGE_VARIABLE = "resultCodeMessage";
     public static final String IS_DIRTY_VARIABLE = "isDirty";
@@ -50,6 +51,13 @@ public class CommonEntities {
         return sqlQuery;
     }
 
+    public static boolean checkInstanceVariable(String instanceVariable) {
+        final boolean isResultCode = Objects.equals(instanceVariable, RESULT_CODE_VARIABLE);
+        final boolean isResultCodeMessage = Objects.equals(instanceVariable, RESULT_CODE_MESSAGE_VARIABLE);
+        final boolean isDirty = Objects.equals(instanceVariable, IS_DIRTY_VARIABLE);
+        final boolean isLog = Objects.equals(instanceVariable, LOG);
+        return isResultCode || isResultCodeMessage || isDirty || isLog;
+    }
     /**
      * Use to get list of parameters in the object
      *
@@ -59,22 +67,21 @@ public class CommonEntities {
     public static List<Object> getParams(Object o) {
         Class<?> cls = o.getClass();
         Method method = null;
-        String[] listNameOfInstanceVariable = null;
-        String nameOfInstanceVariable = null;
+        String[] listOfInstanceVariable = null;
+        String instanceVariable = null;
         List<Object> params = new ArrayList<>();
 
         try {
             for (Field field : cls.getDeclaredFields()) {
-                listNameOfInstanceVariable = field
+                listOfInstanceVariable = field
                         .toString()
                         .split("\\.");
-                nameOfInstanceVariable = listNameOfInstanceVariable[listNameOfInstanceVariable.length - 1];
-                if (Objects.equals(nameOfInstanceVariable, RESULT_CODE_VARIABLE) ||
-                        Objects.equals(nameOfInstanceVariable, RESULT_CODE_MESSAGE_VARIABLE) ||
-                        Objects.equals(nameOfInstanceVariable, IS_DIRTY_VARIABLE) ||
-                        Objects.equals(nameOfInstanceVariable, LOG)) {
+                instanceVariable = listOfInstanceVariable[listOfInstanceVariable.length - 1];
+
+                if (checkInstanceVariable(instanceVariable)) {
+
                 } else {
-                    method = cls.getMethod("get" + nameOfInstanceVariable.substring(0, 1).toUpperCase() + nameOfInstanceVariable.substring(1));
+                    method = cls.getMethod("get" + instanceVariable.substring(0, 1).toUpperCase() + instanceVariable.substring(1));
                     params.add(method.invoke(o));
                 }
             }
@@ -86,29 +93,27 @@ public class CommonEntities {
 
     /**
      * Use to get value after receiving result from Resultset
-     * @param o object
+     *
+     * @param o  object
      * @param rs resultset
      */
     public static void processesParseSql(Object o, ResultSet rs) {
         Class<?> cls = o.getClass();
-        String[] listNameOfInstanceVariable = null;
-        String nameOfInstanceVariable = null;
+        String[] listOfInstanceVariable = null;
+        String instanceVariable = null;
         Method method = null;
 
         try {
             for (Field field : cls.getDeclaredFields()) {
-                Class<?> type = field.getType();
-                listNameOfInstanceVariable = field
+                listOfInstanceVariable = field
                         .toString()
                         .split("\\.");
-                nameOfInstanceVariable = listNameOfInstanceVariable[listNameOfInstanceVariable.length - 1];
-                if (Objects.equals(nameOfInstanceVariable, RESULT_CODE_VARIABLE) ||
-                        Objects.equals(nameOfInstanceVariable, RESULT_CODE_MESSAGE_VARIABLE) ||
-                        Objects.equals(nameOfInstanceVariable, IS_DIRTY_VARIABLE) ||
-                        Objects.equals(nameOfInstanceVariable, LOG)) {
+                instanceVariable = listOfInstanceVariable[listOfInstanceVariable.length - 1];
+                if (checkInstanceVariable(instanceVariable)) {
                 } else {
-                        method = cls.getMethod("set" + nameOfInstanceVariable.substring(0, 1).toUpperCase() + nameOfInstanceVariable.substring(1), type);
-                        invokeMethod(o, method, rs, type, nameOfInstanceVariable);
+                    final Class<?> type = field.getType();
+                    method = cls.getMethod("set" + instanceVariable.substring(0, 1).toUpperCase() + instanceVariable.substring(1), type);
+                    invokeMethod(o, method, rs, type, instanceVariable);
                 }
             }
         } catch (SQLException | InvocationTargetException | NoSuchMethodException | IllegalAccessException e) {
@@ -139,27 +144,25 @@ public class CommonEntities {
 
     /**
      * User to clear value of instance variables
+     *
      * @param o Object
      */
     public static void processClear(Object o) {
         Class<?> cls = o.getClass();
         Method method = null;
-        String[] listNameOfInstanceVariable = null;
-        String nameOfInstanceVariable = null;
+        String[] listOfInstanceVariable = null;
+        String instanceVariable = null;
         try {
             for (Field field : cls.getDeclaredFields()) {
                 Class<?> type = field.getType();
-                listNameOfInstanceVariable = field
+                listOfInstanceVariable = field
                         .toString()
                         .split("\\.");
-                nameOfInstanceVariable = listNameOfInstanceVariable[listNameOfInstanceVariable.length - 1];
-                if (Objects.equals(nameOfInstanceVariable, RESULT_CODE_VARIABLE) ||
-                        Objects.equals(nameOfInstanceVariable, RESULT_CODE_MESSAGE_VARIABLE) ||
-                        Objects.equals(nameOfInstanceVariable, IS_DIRTY_VARIABLE) ||
-                        Objects.equals(nameOfInstanceVariable, LOG)) {
+                instanceVariable = listOfInstanceVariable[listOfInstanceVariable.length - 1];
+                if (checkInstanceVariable(instanceVariable)) {
                 } else {
-                        method = cls.getMethod("set" + nameOfInstanceVariable.substring(0, 1).toUpperCase() + nameOfInstanceVariable.substring(1), type);
-                        invokeMethodClear(o, method, type);
+                    method = cls.getMethod("set" + instanceVariable.substring(0, 1).toUpperCase() + instanceVariable.substring(1), type);
+                    invokeMethodClear(o, method, type);
                 }
             }
         } catch (InvocationTargetException | NoSuchMethodException | IllegalAccessException e) {
@@ -168,7 +171,7 @@ public class CommonEntities {
 
     }
 
-    private  static void invokeMethodClear(Object o, Method method, Class<?> type) throws InvocationTargetException, IllegalAccessException {
+    private static void invokeMethodClear(Object o, Method method, Class<?> type) throws InvocationTargetException, IllegalAccessException {
         if (int.class.equals(type)) {
             method.invoke(o, 0);
         } else if (String.class.equals(type)) {
