21410bc0ed770d56a7cda0af2c3e9ed7509b2be8
NamQuang13 <dragonsangsang10a1@gmail.com>
2022-12-25 13:51:38 +0700

    Message: clean code

--- src/main/java/com/example/Excercise1/repository/RepositoryFuncImpl.java
+++ src/main/java/com/example/Excercise1/repository/RepositoryFuncImpl.java
@@ -1,31 +1,25 @@
 package com.example.Excercise1.repository;
 
-import com.example.Excercise1.exceptions.ConnectionException;
 import com.example.Excercise1.exceptions.SetParameterException;
-import com.example.Excercise1.exceptions.StatementException;
 import com.example.Excercise1.valueObject.Value;
+import com.example.Excercise1.valueUtils.SetValue;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.stereotype.Component;
 
-import javax.sql.DataSource;
-import java.math.BigDecimal;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.Properties;
 import static com.example.Excercise1.constants.MessageException.*;
 
 @Component
 public class RepositoryFuncImpl implements RepositoryFunc {
     private final Environment env;
-    private final DataSource dataSource;
+    private final SetValue setValue;
     @Autowired
-    public RepositoryFuncImpl(Environment env, DataSource dataSource) {
+    public RepositoryFuncImpl(Environment env, SetValue setValue) {
         this.env = env;
-        this.dataSource = dataSource;
+        this.setValue = setValue;
     }
 
     /** Use to set parameter of the object into PreparedStatement
@@ -40,36 +34,10 @@ public class RepositoryFuncImpl implements RepositoryFunc {
         try {
             for (int i = 0, j = 1; i < params.size(); i++, j++) {
                 Object obj = params.get(i);
-                setParam(ps, obj, j);
+                this.setValue.setParamsPreStatement(ps, obj, j);
             }
-        } catch (SQLException | UnsupportedOperationException  e) {
+        } catch (UnsupportedOperationException | SQLException e) {
             throw new SetParameterException(String.format(FAILED_TO_SET_PARAMETER, params), e.getCause());
-        }
-    }
-
-    @Override
-    public void setParam(PreparedStatement ps, Object obj, int j) throws SQLException {
-        if (obj == null) {
-            ps.setString(j, null);
-        } else if (obj instanceof String) {
-            ps.setString(j, (String) obj);
-        } else if (obj instanceof Integer) {
-            ps.setInt(j, (Integer) obj);
-        } else if (obj instanceof Date) {
-            ps.setDate(j, (Date) obj);
-        } else if (obj instanceof BigDecimal) {
-            ps.setBigDecimal(j, (BigDecimal) obj);
-        } else if (obj instanceof Long) {
-            ps.setLong(j, (Long) obj);
-        } else if (obj instanceof Double) {
-            ps.setDouble(j, (Double) obj);
-        } else if (obj instanceof Float) {
-            ps.setFloat(j, (Float) obj);
-        } else if (obj instanceof Short) {
-            ps.setFloat(j, (Short) obj);
-        }
-        else {
-            throw new UnsupportedOperationException("The type " + obj.getClass().getName() + " is currently not supported.");
         }
     }
 
